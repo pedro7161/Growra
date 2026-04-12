8e9faa4663ba752e89976189d2d1d44dc69a61f9
pedro7161 <pedro.ramos9@hotmail.com>
2026-04-12 10:56:50 +0100

    chore(growra): add tests, jest config, mocks and lint/format fixes

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    ,
+    '^react-native$': '<rootDir>/__mocks__/react-native.js'
+  }
+};
